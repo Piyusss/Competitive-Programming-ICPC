a5a05daf375dbfa71d369f7b1ae5f96c6a18a64a
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-11-23 21:40:55 +0530

    Update Binary Lifting for Kth-Ancestor.cpp

--- GRAPH/Binary Lifting for Kth-Ancestor.cpp
+++ GRAPH/Binary Lifting for Kth-Ancestor.cpp
@@ -7,25 +7,25 @@ vector<vector<int>> adj;
 int up[mxn][mxe]; 
 bool vis[mxn];
 
-void dfs(int v, int p) { // keep track of current node and its parent node
-    up[v][0] = p; // mark the parent in the array
-    vis[v] = 1; // mark the node as visited
-    for(int u: adj[v]) if(!vis[u]) dfs(u,v); // visit all unvisited children
+void dfs(int v, int p) {
+    up[v][0] = p;
+    vis[v] = 1;
+    for(int u: adj[v]) if(!vis[u]) dfs(u,v);
 }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
      
     cin >> n >> q; 
-    adj.resize(n); // start the adjacency list with space for n nodes
+    adj.resize(n);
     
-    for(int i = 0; i < n - 1; ++i) { // get in n - 1 edges
+    for(int i = 0; i < n - 1; ++i) {
         int a,b; cin >> a >> b, --a, --b; 
         adj[a].emplace_back(b), adj[b].emplace_back(a); 
     } 
     
-    memset(up, -1, sizeof(up)); // memset up to -1 to begin
-    dfs(0,-1); // fill out up base cases
+    memset(up, -1, sizeof(up));
+    dfs(0,-1);
     
     for(int l = 1; l < mxe; ++l) 
         for(int i = 0; i < n; ++i) 
@@ -35,7 +35,7 @@ int main() {
         int node, k; cin >> node >> k, --node; 
         for(int l = 0; l < mxe; ++l) 
             if(node != -1) if(k & (1 << l)) 
-                node = up[node][l]; // jump up by the power of 2 at this point
-        cout << ++node << "\n"; // renew one-indexing
+                node = up[node][l];
+        cout << ++node << "\n";
     } 
 }
