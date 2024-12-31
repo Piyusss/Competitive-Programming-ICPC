400ebf60971d3f2f67d9dd8fcf8ed34dbe572d85
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-12-31 20:48:14 +0530

    Update Mail Delivery.cpp

--- CSES/GRAPH/Mail Delivery.cpp
+++ CSES/GRAPH/Mail Delivery.cpp
@@ -5,57 +5,67 @@ using ll = long long;
 
 //--------------------------------WRITE_HERE-------------------------------------------
 
-vector<ll>res;
+vector<ll> res;
 
-void eulerianDfs(ll node,vector<vector<pair<ll,ll>>>&g,vector<ll>&deg,vector<bool>&seen){
-    while(!g[node].empty()){
-        ll child=g[node].back().first;
-        ll idx=g[node].back().second;
-        
+void eulerianDfs(ll node, vector<vector<pair<ll, ll>>> &g, vector<bool> &seen)
+{
+    while (!g[node].empty())
+    {
+        ll child = g[node].back().first;
+        ll idx = g[node].back().second;
+
         g[node].pop_back();
-        if(seen[idx]) continue;
+        if (seen[idx])
+            continue;
 
-        seen[idx]=1;
-        eulerianDfs(child,g,deg,seen);
+        seen[idx] = 1;
+        eulerianDfs(child, g, seen);
     }
     res.push_back(node);
 }
 
 void _144()
 {
-    ll n,m;
-    cin>>n>>m;
+    ll n, m;
+    cin >> n >> m;
 
-    vector<vector<pair<ll,ll>>>g(n);
-    vector<ll>deg(n);
-    vector<bool>seen(m);
-    
-    for(ll i=0;i<m;i+=1){
-        ll a,b;
-        cin>>a>>b;
+    vector<vector<pair<ll, ll>>> g(n);
+    vector<ll> deg(n);
+    vector<bool> seen(m);
 
-        a-=1;b-=1;
-        deg[a]+=1;deg[b]+=1;
-        g[a].push_back({b,i});
-        g[b].push_back({a,i});
+    for (ll i = 0; i < m; i += 1)
+    {
+        ll a, b;
+        cin >> a >> b;
+
+        a -= 1;
+        b -= 1;
+        deg[a] += 1;
+        deg[b] += 1;
+        g[a].push_back({b, i});
+        g[b].push_back({a, i});
     }
 
-    for(ll i=0;i<n;i+=1){
-        if(deg[i]&1){
-            cout<<"IMPOSSIBLE";
+    for (ll i = 0; i < n; i += 1)
+    {
+        if (deg[i] & 1)
+        {
+            cout << "IMPOSSIBLE";
             return;
         }
     }
 
-    eulerianDfs(0,g,deg,seen);
+    eulerianDfs(0, g, seen);
 
-    if(m+1 != res.size()){
-        cout<<"IMPOSSIBLE";
+    if (m + 1 != res.size())
+    {
+        cout << "IMPOSSIBLE";
         return;
     }
 
-    for(auto &it:res){
-        cout<<it+1<<" ";
+    for (auto &it : res)
+    {
+        cout << it + 1 << " ";
     }
 
     return;
