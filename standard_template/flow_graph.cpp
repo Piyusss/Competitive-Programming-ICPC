f39c7a372bb2cc51e236f63869af6878ab190f90
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2025-01-02 21:37:17 +0530

    Update flow_graph.cpp

--- standard_template/flow_graph.cpp
+++ standard_template/flow_graph.cpp
@@ -1,74 +1,115 @@
-struct flow_network {
+typedef pair<ll, ll> pii;
+const ll INF = 0x3f3f3f3f;
 
-    struct edge {
-        int u, v, cap;
-        edge *rev;
-        bool forward;
-        edge(int _u, int _v, int _cap, bool forw)
-            : u(_u), v(_v), cap(_cap), forward(forw) { }
-    };
+class FlowGraph
+{
+private:
+    ll N, M;
+    vector<vector<ll>> capacity;
+    vector<vector<ll>> adjList;
+    vector<vector<pii>> edgeList;
+    vector<ll> parent;
+    vector<bool> visitedEdges;
 
-    int n;
-    vector<vector<edge*> > adj;
-    flow_network(int _n) : n(_n), adj(_n) { }
-
-    void add_edge(int u, int v, int cap) {
-        edge *e = new edge(u, v, cap, true);
-        edge *rev = new edge(v, u, 0, false);
-        e->rev = rev;
-        rev->rev = e;
-        adj[u].push_back(e);
-        adj[v].push_back(rev);
+public:
+    FlowGraph(ll n, ll m) : N(n), M(m)
+    {
+        capacity.assign(N + 1, vector<ll>(N + 1, 0));
+        adjList.assign(N + 1, vector<ll>());
+        edgeList.assign(N + 1, vector<pii>());
+        parent.assign(N + 1, -1);
+        visitedEdges.assign(M, false);
     }
 
-    int augment(int s, int t) {
-        vector<edge*> back(n, (edge*)0);
-        queue<int> Q;
-        Q.push(s);
-        back[s] = (edge*)1;
-        while (!Q.empty()) {
-            int u = Q.front(); Q.pop();
-            for (int i = 0; i < adj[u].size(); i++) {
-                int v = adj[u][i]->v;
-                if (back[v] == NULL && adj[u][i]->cap > 0) {
-                    back[v] = adj[u][i];
-                    Q.push(v);
+    void addEdge(ll a, ll b, ll weight, ll id)
+    {
+        edgeList[a].push_back({b, id});
+        adjList[a].push_back(b);
+        adjList[b].push_back(a);
+        capacity[a][b] += weight;
+    }
+
+    ll bfs(ll s, ll t)
+    {
+        fill(parent.begin(), parent.end(), -1);
+        parent[s] = -2;
+
+        queue<pii> q;
+        q.push({s, INF});
+
+        while (!q.empty())
+        {
+            ll u = q.front().first;
+            ll flow = q.front().second;
+            q.pop();
+
+            for (ll v : adjList[u])
+            {
+                if (parent[v] == -1 && capacity[u][v] > 0)
+                {
+                    parent[v] = u;
+                    ll aug = min(flow, capacity[u][v]);
+                    if (v == t)
+                        return aug;
+                    q.push({v, aug});
                 }
             }
         }
-
-        if (back[t] == NULL)
-            return 0;
-
-        stack<edge*> S;
-        S.push(back[t]);
-        int bneck = back[t]->cap;
-        while (S.top()->u != s) {
-            S.push(back[S.top()->u]);
-            bneck = min(bneck, S.top()->cap);
-        }
-
-        while (!S.empty()) {
-            S.top()->cap -= bneck;
-            S.top()->rev->cap += bneck;
-            S.pop();
-        }
-
-        return bneck;
+        return 0;
     }
 
-    int max_flow(int source, int sink) {
-        int flow = 0;
-        // Repeatedly find augmenting paths and add flow
-        while (true) {
-            int f = augment(source, sink);
-            if (f == 0) {
-                break;
-            }
+    ll maxFlow(ll s, ll t)
+    {
+        ll totalFlow = 0, augFlow;
 
-            flow += f;
+        while ((augFlow = bfs(s, t)) > 0)
+        {
+            totalFlow += augFlow;
+            ll u = t;
+
+            while (u != s)
+            {
+                ll v = parent[u];
+                capacity[v][u] -= augFlow;
+                capacity[u][v] += augFlow;
+                u = v;
+            }
         }
 
-        return flow;
+        return totalFlow;
+    }
+
+    void dfs(ll u, vector<ll> &path, ll t)
+    {
+        path.push_back(u);
+        if (u == t)
+            return;
+
+        for (const auto &edge : edgeList[u])
+        {
+            ll v = edge.first;
+            ll id = edge.second;
+
+            if (capacity[u][v] == 0 && !visitedEdges[id])
+            {
+                visitedEdges[id] = true;
+                dfs(v, path, t);
+                return;
+            }
+        }
+    }
+
+    void printPaths(ll s, ll t, ll maxFlowValue)
+    {
+        for (ll i = 0; i < maxFlowValue; ++i)
+        {
+            vector<ll> path;
+            dfs(s, path, t);
+            cout << path.size() << endl;
+            for (size_t j = 0; j < path.size(); ++j)
+            {
+                cout << path[j]+1 << (j == path.size() - 1 ? '\n' : ' ');
+            }
+        }
     }
 };
