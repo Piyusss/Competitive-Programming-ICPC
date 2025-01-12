2ddc327e47a7a715190f136946e6851f6e3fdf6d
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2025-01-12 18:42:01 +0530

    Create upd point + que 1stEl>=x from >=idx.cpp

---
+++ standard_template/segmentTree/upd point + que 1stEl>=x from >=idx.cpp
@@ -0,0 +1,83 @@
+class SegmentTree
+{
+private:
+    vector<ll> tree;
+    vector<ll> arr;
+    ll n;
+
+    void build(ll node, ll start, ll end)
+    {
+        if (start == end)
+        {
+            tree[node] = arr[start];
+        }
+        else
+        {
+            ll mid = (start + end) / 2;
+            build(2 * node + 1, start, mid);
+            build(2 * node + 2, mid + 1, end);
+            tree[node] = max(tree[2 * node + 1], tree[2 * node + 2]);
+        }
+    }
+
+    void update(ll node, ll start, ll end, ll idx, ll value)
+    {
+        if (start == end)
+        {
+            tree[node] = value;
+            arr[start] = value;
+        }
+        else
+        {
+            ll mid = (start + end) / 2;
+            if (idx <= mid)
+            {
+                update(2 * node + 1, start, mid, idx, value);
+            }
+            else
+            {
+                update(2 * node + 2, mid + 1, end, idx, value);
+            }
+            tree[node] = max(tree[2 * node + 1], tree[2 * node + 2]);
+        }
+    }
+
+    ll first_above(ll node, ll start, ll end, ll x, ll l)
+    {
+        if (tree[node] < x || end < l)
+        {
+            return -1;
+        }
+        if (start == end)
+        {
+            return start;
+        }
+        ll mid = (start + end) / 2;
+        if (mid >= l && tree[2 * node + 1] >= x)
+        {
+            ll left_result = first_above(2 * node + 1, start, mid, x, l);
+            if (left_result != -1)
+                return left_result;
+        }
+        return first_above(2 * node + 2, mid + 1, end, x, l);
+    }
+
+public:
+    SegmentTree(const vector<ll> &input)
+    {
+        n = input.size();
+        arr = input;
+        tree.resize(4 * n, 0);
+        build(0, 0, n - 1);
+    }
+
+    void set(ll idx, ll value)
+    {
+        update(0, 0, n - 1, idx, value);
+    }
+
+    ll first_above(ll x, ll l)
+    {
+        return first_above(0, 0, n - 1, x, l);
+    }
+};
