f8c6dc93b9261e6d008d159cb1c0fba76b74a10c
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2025-01-14 20:13:07 +0530

    Update Fixed-Length Paths I.cpp

--- CSES/TREE/Fixed-Length Paths I.cpp
+++ CSES/TREE/Fixed-Length Paths I.cpp
@@ -4,99 +4,99 @@ using namespace std;
 using ll = long long;
 
 //--------------------------------WRITE_HERE-------------------------------------------
-int n, k, a, b;
-vector<int> g[200000 + 1];
-vector<int> subtree_size(200000 + 1);
-vector<int> processed(200000 + 1);
-int cnt[200001]{1}, max_depth;
-ll res = 0;
 
-void get_cnt(int node, int p, bool filling, int depth = 1)
+int n, a, b;
+vector<int> graph[200001];
+int subtree[200001];
+
+ll ans = 0, bit[200001];
+int total_cnt[200001]{1}, mx_depth;
+int cnt[200001], subtree_depth;
+bool processed[200001];
+
+int get_subtree_sizes(int node, int parent = 0)
 {
-    if (depth > k)
-        return;
-
-    max_depth = max(max_depth, depth);
-    if (filling)
-        cnt[depth]++;
-    else
-        res += cnt[k - depth];
-
-    for (int &i : g[node])
-    {
-        if (!processed[i] && i != p)
-        {
-            get_cnt(i, node, filling, depth + 1);
-        }
-    }
+    subtree[node] = 1;
+    for (int i : graph[node])
+        if (!processed[i] && i != parent)
+            subtree[node] += get_subtree_sizes(i, node);
+    return subtree[node];
 }
 
-int get_subtree_size(int node, int p)
+int get_centroid(int desired, int node, int parent = 0)
 {
-    subtree_size[node] = 1;
-    for (int &c : g[node])
-    {
-        if (c != p && !processed[c])
-        {
-            subtree_size[node] += get_subtree_size(c, node);
-            ;
-        }
-    }
-
-    return subtree_size[node];
-}
-
-int centroidDfs(int node, int p, int desired)
-{
-    for (int &c : g[node])
-    {
-        if (c != p && !processed[c])
-        {
-            if (subtree_size[c] >= desired)
-            {
-                return centroidDfs(c, node, desired);
-            }
-        }
-    }
-
+    for (int i : graph[node])
+        if (!processed[i] && i != parent && subtree[i] >= desired)
+            return get_centroid(desired, i, node);
     return node;
 }
 
-void centroid_decomposition(int node)
+void get_cnt(int node, int parent, int depth = 1)
 {
-    int centroid = centroidDfs(node, -1, get_subtree_size(node, -1) >> 1);
-    processed[centroid] = 1;
-    max_depth = 0;
+    if (depth > b)
+        return;
+    subtree_depth = max(subtree_depth, depth);
+    cnt[depth]++;
+    for (int i : graph[node])
+        if (!processed[i] && i != parent)
+            get_cnt(i, node, depth + 1);
+}
 
-    for (int &c : g[centroid])
-    {
-        if (!processed[c])
+void centroid_decomp(int node = 1)
+{
+    int centroid = get_centroid(get_subtree_sizes(node) >> 1, node);
+    processed[centroid] = true;
+    mx_depth = 0;
+    long long partial_sum_init = (a == 1 ? 1ll : 0ll);
+    for (int i : graph[centroid])
+        if (!processed[i])
         {
-            get_cnt(c, centroid, 0);
-            get_cnt(c, centroid, 1);
+            subtree_depth = 0;
+            get_cnt(i, centroid);
+
+            long long partial_sum = partial_sum_init;
+            for (int depth = 1; depth <= subtree_depth; depth++)
+            {
+                ans += partial_sum * cnt[depth];
+
+                int dremove = b - depth;
+                if (dremove >= 0)
+                    partial_sum -= total_cnt[dremove];
+                int dadd = a - (depth + 1);
+                if (dadd >= 0)
+                    partial_sum += total_cnt[dadd];
+            }
+
+            for (int depth = a - 1; depth <= b - 1 && depth <= subtree_depth; depth++)
+                partial_sum_init += cnt[depth];
+
+            for (int depth = 1; depth <= subtree_depth; depth++)
+                total_cnt[depth] += cnt[depth];
+            mx_depth = max(mx_depth, subtree_depth);
+
+            fill(cnt, cnt + subtree_depth + 1, 0);
         }
-    }
-    fill(cnt + 1, cnt + 1 + max_depth, 0);
-    for (int &c : g[centroid])
-    {
-        if (!processed[c])
-            centroid_decomposition(c);
-    }
+    fill(total_cnt + 1, total_cnt + mx_depth + 1, 0);
+    for (int i : graph[centroid])
+        if (!processed[i])
+            centroid_decomp(i);
 }
 
 void _144()
 {
+    int k;
     cin >> n >> k;
-
-    for (int i = 0; i < n - 1; i++)
+    a = k;
+    b = k;
+    for (int i = 1; i < n; i++)
     {
-        cin >> a >> b;
-        g[a].push_back(b);
-        g[b].push_back(a);
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
     }
-
-    centroid_decomposition(1);
-    cout << res;
+    centroid_decomp();
+    cout << ans;
 
     return;
 }
@@ -113,7 +113,7 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int t = 1;
+    ll t = 1;
     // cin >> t;
     while (t--)
     {
