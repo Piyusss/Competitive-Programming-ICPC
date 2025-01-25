3f24ec83da4ab4bfb96cc64b1852255ab25a6a4a
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2025-01-25 16:02:22 +0530

    Update Tree Diameter.cpp

--- CSES/TREE/Tree Diameter.cpp
+++ CSES/TREE/Tree Diameter.cpp
@@ -1,5 +1,86 @@
-//Twice-dfs-Method
+//Method 01: Dp on Trees
+//@memset.DP.-1
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
 
+//--------------------------------WRITE_HERE-------------------------------------------
+
+const int MAXN=200000+1;
+vector<int>g[MAXN];
+int res=0;
+int dp[MAXN][2];
+
+void dfs(int node,int p){
+    vector<int>all;
+
+    for(auto &c:g[node]){
+        if(c!=p){
+            dfs(c,node);
+            all.push_back(dp[c][0]);
+        }
+    }
+
+    sort(all.rbegin(),all.rend());
+    int sz=all.size();
+
+    if(sz>=1){
+        dp[node][0]=all[0]+1;
+    }
+    if(sz>=2){
+        dp[node][1]=(all[0]+1) + (1+all[1]);
+    }
+
+    res=max(res,max(dp[node][0],dp[node][1]));
+}
+
+void _144()
+{
+    int n;cin>>n;
+    memset(dp,0,sizeof(dp));
+
+    for(int i=0;i<n-1;i++){
+        int u,v;
+        cin>>u>>v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    dfs(1,-1);
+
+    cout<<res;
+    return;
+}
+
+//--------------------------------END--------------------------------------------------
+
+// Main
+int main()
+{
+//freopen("input.in", "r",stdin);
+//freopen("output.out", "w",stdout);
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        _144();
+    }
+}
+
+
+
+
+
+
+
+
+
+//Method-02: Twice Dfs
 //@memset.DP.-1
 #include <bits/stdc++.h>
 using namespace std;
